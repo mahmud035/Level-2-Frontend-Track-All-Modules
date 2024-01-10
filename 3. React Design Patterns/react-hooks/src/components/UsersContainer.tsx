9a6860ea7c9d6f5a545632c8ea493223584ab360
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-01-10 21:01:09 +0600

    3-8 Component Composition With Hooks (Custom Hook)

--- 3. React Design Patterns/react-hooks/src/components/UsersContainer.tsx
+++ 3. React Design Patterns/react-hooks/src/components/UsersContainer.tsx
@@ -1,32 +1,9 @@
-import { useEffect, useState } from 'react';
+import useUsersData from '../hooks/useUsersData';
 import UserList from './UserList';
 
 // NOTE: Container Component
 const UsersContainer = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [data, setData] = useState([]);
-
-  const url = 'https://randomuser.me/api/?results=15';
-
-  const getUsers = async () => {
-    setIsLoading(true);
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      setIsLoading(false);
-      setData(data.results);
-    } catch (error) {
-      setError(true);
-    }
-  };
-
-  // WARNING: MUST call the function inside useEffect.
-  useEffect(() => {
-    getUsers();
-  }, []);
-
-  // console.log(data);
+  const { isLoading, error, data } = useUsersData(); // custom hook
 
   return <UserList isLoading={isLoading} error={error} data={data} />;
 };
---
+++ 3. React Design Patterns/react-hooks/src/hooks/useUsersData.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
+
+const useUsersData = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState([]);
+  const controller = new AbortController();
+  const signal = controller.signal;
+
+  const url = 'https://randomuser.me/api/?results=15';
+
+  const getUsers = async () => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(url, { signal });
+      const data = await res.json();
+      setIsLoading(false);
+      setData(data.results);
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  // WARNING: MUST call the function inside useEffect.
+  useEffect(() => {
+    getUsers();
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  return { isLoading, error, data };
+};
+
+export default useUsersData;
