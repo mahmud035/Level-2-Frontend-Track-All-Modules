ae82a93d2bed04e19a7bdef952bd776b8ede6813
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-01-15 14:44:45 +0600

    4-4 Tailwind Custom Class And Class Merge Issue

--- 4. Reusable Components/reusable-components/src/pages/Home.tsx
+++ 4. Reusable Components/reusable-components/src/pages/Home.tsx
@@ -1,12 +1,14 @@
+import Header from '../shared/Header';
+
 const Home = () => {
   return (
     <div>
-      <h1 className="py-10 text-3xl">Home Page</h1>
+      <h1 className="py-10">Home Page</h1>
+      <Header />
 
       <div className="flex items-center justify-center w-full h-screen gap-4">
-        <button className="btn-primary">This is a button</button>
-        <button className="btn-primary">This is a button</button>
-        <button className="btn-primary">This is a button</button>
+        <button className="btn btn-primary">This is a button</button>
+        <button className="btn btn-danger">This is a button</button>
       </div>
     </div>
   );
--- 4. Reusable Components/reusable-components/src/shared/Footer.tsx
+++ 4. Reusable Components/reusable-components/src/shared/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="w-full h-16 text-center bg-gray-400 rounded-md">
+    <div className="w-full h-16 text-center bg-gray-300 rounded-md">
       <h1>Footer</h1>
     </div>
   );
