51174b45af281f4cc08eec19defa4accefe1b4da
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-01-18 07:49:41 +0600

    5-3 Conditional Single-Column Form Layout

--- 5. Forms in React/reusable-form/src/components/NormalForm/NormalForm.tsx
+++ 5. Forms in React/reusable-form/src/components/NormalForm/NormalForm.tsx
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form';
+import cn from '../../utils/cn';
 
 const NormalForm = () => {
   const {
@@ -11,44 +12,56 @@ const NormalForm = () => {
     console.log(data);
   };
 
+  const double = true;
+
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="max-w-5xl p-5 mx-auto border border-red-500"
+      className={cn('w-full p-5 mx-auto border border-red-500', {
+        'max-w-5xl': double,
+        'max-w-md': !double,
+      })}
     >
-      <div className="grid grid-cols-2 gap-5 border border-blue-500 ">
-        <div className="w-full">
+      <div
+        className={cn(
+          'grid grid-cols-1 gap-5 border border-blue-500 justify-items-center',
+          {
+            'md:grid-cols-2': double,
+          }
+        )}
+      >
+        <div className="w-full max-w-md">
           <label htmlFor="name" className="block font-medium">
             Name
           </label>
           <input
             type="text"
             id="name"
-            className="w-full rounded"
+            className="w-full border border-gray-300 rounded-md focus:border-purple-500 focus:ring-1 focus:ring-purple-500"
             {...register('name')}
           />
         </div>
 
-        <div className="w-full">
+        <div className="w-full max-w-md">
           <label htmlFor="email" className="block font-medium">
             Email
           </label>
           <input
             type="text"
             id="name"
-            className="w-full rounded"
+            className="w-full border border-gray-300 rounded-md focus:border-purple-500 focus:ring-1 focus:ring-purple-500"
             {...register('email')}
           />
         </div>
 
-        <div className="w-full">
+        <div className="w-full max-w-md">
           <label htmlFor="password" className="block font-medium">
             Password
           </label>
           <input
             type="text"
             id="name"
-            className="w-full rounded"
+            className="w-full border border-gray-300 rounded-md focus:border-purple-500 focus:ring-1 focus:ring-purple-500"
             {...register('password')}
           />
         </div>
