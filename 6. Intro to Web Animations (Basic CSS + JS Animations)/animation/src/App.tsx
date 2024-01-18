b8eca4950a131fb751bfc682f31d21aa57ffd296
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-01-18 22:00:47 +0600

    6-3 3D Transform Element

--- 6. Intro to Web Animations (Basic CSS + JS Animations)/animation/src/App.tsx
+++ 6. Intro to Web Animations (Basic CSS + JS Animations)/animation/src/App.tsx
@@ -4,9 +4,6 @@ function App() {
   return (
     <div className="main">
       <div className="box-1"></div>
-      <div className="box-container-2">
-        <div className="box-2"></div>
-      </div>
     </div>
   );
 }
