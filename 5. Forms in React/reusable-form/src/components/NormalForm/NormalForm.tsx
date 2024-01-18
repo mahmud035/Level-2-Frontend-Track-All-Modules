cb808f6286b59dc90e3db990f7c68fc8bc3590f4
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-01-18 07:28:55 +0600

    5-2 Double Column From Layout

--- 5. Forms in React/reusable-form/src/components/NormalForm/NormalForm.tsx
+++ 5. Forms in React/reusable-form/src/components/NormalForm/NormalForm.tsx
@@ -12,15 +12,49 @@ const NormalForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label className="block" htmlFor="name">
-          Name
-        </label>
-        <input type="text" id="name" {...register('name')} />
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="max-w-5xl p-5 mx-auto border border-red-500"
+    >
+      <div className="grid grid-cols-2 gap-5 border border-blue-500 ">
+        <div className="w-full">
+          <label htmlFor="name" className="block font-medium">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="w-full rounded"
+            {...register('name')}
+          />
+        </div>
+
+        <div className="w-full">
+          <label htmlFor="email" className="block font-medium">
+            Email
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="w-full rounded"
+            {...register('email')}
+          />
+        </div>
+
+        <div className="w-full">
+          <label htmlFor="password" className="block font-medium">
+            Password
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="w-full rounded"
+            {...register('password')}
+          />
+        </div>
       </div>
 
-      <button type="submit">Submit</button>
+      {/* <Button>Submit</Button> */}
     </form>
   );
 };
