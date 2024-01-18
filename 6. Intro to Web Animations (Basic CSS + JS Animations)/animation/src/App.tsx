50463b50bedbfcbed0e99b8b0c7dc5911a128bde
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-01-18 21:16:24 +0600

    6-1 Element Movement On X And Y Axis Using Translate

---
+++ 6. Intro to Web Animations (Basic CSS + JS Animations)/animation/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="main">
+      <div className="box-1"></div>
+      <h1>Something cool</h1>
+    </div>
+  );
+}
+
+export default App;
