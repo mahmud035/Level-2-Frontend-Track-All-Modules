e5ef5e298ae987d9aad121706fee451ca40e4f43
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-01-18 23:17:27 +0600

    6-7 CSS Clip Path Transition (Fun & Interesting)

--- 6. Intro to Web Animations (Basic CSS + JS Animations)/animation/src/App.tsx
+++ 6. Intro to Web Animations (Basic CSS + JS Animations)/animation/src/App.tsx
@@ -3,9 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="main">
-      <div className="loading">
-        <div className="glass"></div>
-        <div className="box"></div>
+      <div className="card">
+        <h1>Heading</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae ratione
+          libero, quasi doloribus, assumenda facere odio repellat esse debitis
+          inventore voluptates laborum. Officia et quas est tenetur debitis ab.
+          Omnis.
+        </p>
       </div>
     </div>
   );
