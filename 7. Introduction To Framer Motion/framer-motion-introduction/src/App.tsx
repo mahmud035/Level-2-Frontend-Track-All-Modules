4573a8aee6a8401e099f6105e7349f4d8a2cb5e5
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-01-21 08:35:23 +0600

    7-1 Motion Element

---
+++ 7. Introduction To Framer Motion/framer-motion-introduction/src/App.tsx
@@ -0,0 +1,11 @@
+import Lesson1 from './pages/Lesson1';
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-screen">
+      <Lesson1 />
+    </div>
+  );
+};
+
+export default App;
---
+++ 7. Introduction To Framer Motion/framer-motion-introduction/src/pages/Lesson1.tsx
@@ -0,0 +1,20 @@
+import { motion } from 'framer-motion';
+
+const initial = { rotate: 0 };
+const animate = { rotate: 360 };
+const transition = { type: 'spring', duration: 2 };
+
+const Lesson1 = () => {
+  return (
+    <div>
+      <motion.div
+        className="bg-indigo-500 rounded-lg size-64"
+        initial={initial}
+        animate={animate}
+        transition={transition}
+      ></motion.div>
+    </div>
+  );
+};
+
+export default Lesson1;
