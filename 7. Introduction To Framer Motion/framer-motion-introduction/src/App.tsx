046c0f8a7db413232e4b0184f45f09d02cecb7c1
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-01-21 11:06:09 +0600

    7-4 Gesture - Drag

--- 7. Introduction To Framer Motion/framer-motion-introduction/src/App.tsx
+++ 7. Introduction To Framer Motion/framer-motion-introduction/src/App.tsx
@@ -1,13 +1,15 @@
 // import Lesson1 from './pages/Lesson1';
 // import Lesson2 from './pages/Lesson2';
-import Lesson3 from './pages/Lesson3';
+// import Lesson3 from './pages/Lesson3';
+import Lesson4 from './pages/Lesson4';
 
 const App = () => {
   return (
     <div className="flex items-center justify-center w-full h-screen">
       {/* <Lesson1 /> */}
       {/* <Lesson2 /> */}
-      <Lesson3 />
+      {/* <Lesson3 /> */}
+      <Lesson4 />
     </div>
   );
 };
---
+++ 7. Introduction To Framer Motion/framer-motion-introduction/src/pages/Lesson4.tsx
@@ -0,0 +1,50 @@
+import { motion } from 'framer-motion';
+import { useRef } from 'react';
+
+const parent = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: {
+    opacity: 0.6,
+    scale: 1,
+    transition: {
+      ease: 'easeInOut',
+      duration: 1.5,
+    },
+  },
+  hover: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  tap: {
+    scale: 1.1,
+    boxShadow: '0px 10px 10px #000',
+  },
+};
+
+const Lesson4 = () => {
+  const parentRef = useRef(null);
+
+  return (
+    <motion.div
+      ref={parentRef}
+      className="flex items-center justify-center border-2 border-red-500 rounded-lg size-3/6 "
+    >
+      <motion.div
+        className="flex flex-wrap items-center justify-center gap-4 p-5 bg-indigo-500 rounded-lg size-64"
+        variants={parent}
+        initial="hidden"
+        animate="visible"
+        whileHover="hover"
+        drag
+        dragConstraints={parentRef}
+        dragSnapToOrigin={true}
+        dragElastic={0.7}
+        whileDrag="tap"
+      ></motion.div>
+    </motion.div>
+  );
+};
+
+export default Lesson4;
