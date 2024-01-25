af7f895a032379ca5b964993bdd24e6db1f2bf8b
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-01-25 19:17:37 +0600

    8-4 Tailwind And Shadcn Customization

--- 8. Responsive Landing Page and Framer Motion/module-8/src/components/ui/layouts/Navbar.tsx
+++ 8. Responsive Landing Page and Framer Motion/module-8/src/components/ui/layouts/Navbar.tsx
@@ -4,14 +4,12 @@ import { Button } from '../button';
 const Navbar = () => {
   return (
     <header className="h-16">
-      <nav className="flex items-center justify-between w-full h-full max-w-[1250px] px-5 mx-auto font-medium bg-green-500">
+      <nav className="flex items-center justify-between w-full h-full max-w-[1250px] px-5 mx-auto font-medium">
         <span className="text-3xl">iRepair</span>
         <ul className="flex items-center gap-8">
           <NavLink to="/">Home</NavLink>
           <NavLink to="/about">About</NavLink>
-          <Button>
-            <NavLink to="/login">Login</NavLink>
-          </Button>
+          <Button>Login</Button>
         </ul>
       </nav>
     </header>
