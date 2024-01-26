c33e1098dc36528a0884f611f70b5ddd5c6a16e0
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-01-26 10:25:08 +0600

    8-6 Responsive Hero Section

--- 8. Responsive Landing Page and Framer Motion/module-8/src/pages/Home/HeroSection.tsx
+++ 8. Responsive Landing Page and Framer Motion/module-8/src/pages/Home/HeroSection.tsx
@@ -6,18 +6,26 @@ const HeroSection = () => {
   return (
     <Container className="grid grid-cols-1 lg:grid-cols-2 h-[calc(100vh-64px)] place-content-center ">
       <div className="-ml-2">
-        <h1 className="font-bold text-8xl">
+        <h1 className="text-5xl font-bold lg:text-8xl text-nowrap">
           <span className="text-gray">Don't worry.</span>
           <br />
-          <span className="text-dark-gray">We'll fix it.</span>
+          <span>We'll fix it.</span>
         </h1>
-        <p>
-          Welcome to <span>iRepair</span>, your one-stop place for all kinds of
-          <span>Macbook repairs</span> and diagnostics.
+        <p className="mt-10 max-w-[50ch] mb-5 text-lg text-dark-gray">
+          Welcome to{' '}
+          <span className="font-semibold text-primary-foreground">
+            iRepair,{' '}
+          </span>
+          your one-stop place for all kinds of
+          <span className="font-semibold text-primary-foreground">
+            {' '}
+            Macbook repairs{' '}
+          </span>
+          and diagnostics.
         </p>
         <Button>Book a service</Button>
       </div>
-      <div className="-mr-2">
+      <div className="w-3/4 mx-auto mt-10 lg:-mr-2 lg:w-full">
         <img
           src={macBook}
           alt=""
