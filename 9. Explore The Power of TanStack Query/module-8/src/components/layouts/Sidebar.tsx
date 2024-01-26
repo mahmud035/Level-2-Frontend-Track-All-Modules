c07925f4aa707936c6f949fbfd147d6c84035f56
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-01-26 17:18:03 +0600

    9-2 Responsive Sidebar

--- 9. Explore The Power of TanStack Query/module-8/src/components/layouts/Sidebar.tsx
+++ 9. Explore The Power of TanStack Query/module-8/src/components/layouts/Sidebar.tsx
@@ -1,17 +1,53 @@
+import { cn } from '@/lib/utils';
+import { LayoutDashboard } from 'lucide-react';
 import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-    <aside className="sticky top-0 left-0 h-screen col-span-2 p-5 overflow-auto bg-light-gray">
-      <nav className="flex flex-col">
+    <aside className="sticky top-0 left-0 h-screen col-span-2 p-4 overflow-auto lg:p-5 bg-light-gray">
+      <nav className="flex flex-col gap-2">
         <NavLink
-          to="/admin"
-          className="p-3 text-center truncate transition-all rounded-md bg-gray hover:bg-dark-gray hover:text-white"
+          to="/admin/dashboard"
+          className={({ isActive }) =>
+            cn(
+              'flex items-center gap-2 p-3 font-medium transition-all rounded-md bg-gray hover:bg-dark-gray hover:text-white',
+              {
+                'bg-dark-gray text-white': isActive,
+              }
+            )
+          }
         >
-          Dashboard
+          <LayoutDashboard className="shrink-0" />
+          <span className="truncate">Dashboard</span>
         </NavLink>
-        <NavLink to="/admin/service-list">Service List</NavLink>
-        <NavLink to="/admin/add-service">Add Service</NavLink>
+        <NavLink
+          to="/admin/service-list"
+          className={({ isActive }) =>
+            cn(
+              'flex items-center gap-2 p-3 font-medium transition-all rounded-md bg-gray hover:bg-dark-gray hover:text-white',
+              {
+                'bg-dark-gray text-white': isActive,
+              }
+            )
+          }
+        >
+          <LayoutDashboard className="shrink-0" />
+          <span className="truncate">Service List</span>
+        </NavLink>
+        <NavLink
+          to="/admin/add-service"
+          className={({ isActive }) =>
+            cn(
+              'flex items-center gap-2 p-3 font-medium transition-all rounded-md bg-gray hover:bg-dark-gray hover:text-white',
+              {
+                'bg-dark-gray text-white': isActive,
+              }
+            )
+          }
+        >
+          <LayoutDashboard className="shrink-0" />
+          <span className="truncate">Add Service</span>
+        </NavLink>
       </nav>
     </aside>
   );
