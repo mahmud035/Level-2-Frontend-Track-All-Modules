8fc4b51ae6c792b6d7fcc326026494c745c2463d
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-01-26 12:24:12 +0600

    8-11 Responsive Tiles

---
+++ 8. Responsive Landing Page and Framer Motion/module-8/src/pages/Home/ServicesSection.tsx
@@ -0,0 +1,27 @@
+import Container from '@/components/Container';
+
+const ServicesSection = () => {
+  return (
+    <Container className="my-40">
+      <div className="flex flex-col items-center justify-center text-center">
+        <h1>Services that we provide </h1>
+        <p className="max-w-[80ch] mt-10 mb-20">
+          All out technicians are fully qualified and licensed. Moreover, they
+          are incredibly skillful and proficient in various aspects of computer
+          repair.
+        </p>
+      </div>
+      <div className="grid grid-cols-12 gap-5 ">
+        <div className="h-[415px] col-span-12 bg-red-500 rounded-lg "></div>
+        <div className="h-[415px] col-span-12 md:col-span-6 lg:col-span-5 bg-red-500 rounded-lg"></div>
+        <div className="h-[415px] col-span-12 md:col-span-6 lg:col-span-7 bg-red-500 rounded-lg"></div>
+        <div className="h-[415px] col-span-12 bg-red-500 rounded-lg "></div>
+        <div className="h-[415px] col-span-12 md:col-span-6 lg:col-span-4 bg-red-500 rounded-lg"></div>
+        <div className="h-[415px] col-span-12 md:col-span-6 lg:col-span-4 bg-red-500 rounded-lg"></div>
+        <div className="h-[415px] col-span-12 lg:col-span-4 bg-red-500 rounded-lg"></div>
+      </div>
+    </Container>
+  );
+};
+
+export default ServicesSection;
--- 8. Responsive Landing Page and Framer Motion/module-8/src/pages/Home/index.tsx
+++ 8. Responsive Landing Page and Framer Motion/module-8/src/pages/Home/index.tsx
@@ -1,11 +1,12 @@
 import AboutUsSection from './AboutUsSection';
 import HeroSection from './HeroSection';
+import ServicesSection from './ServicesSection';
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
-
+      <ServicesSection />
       <AboutUsSection />
     </div>
   );
