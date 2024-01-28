1a93db09c58d28be20cc95d04bd3fce0d48df5a9
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-01-28 19:06:12 +0600

    9-10 Caching In TanStack Query (Real-time data fetching vibe)

--- 9. Explore The Power of TanStack Query/module-8/src/pages/admin/Dashboard.tsx
+++ 9. Explore The Power of TanStack Query/module-8/src/pages/admin/Dashboard.tsx
@@ -1,5 +1,5 @@
 const Dashboard = () => {
-  return <div>Dashboard component</div>;
+  return <h3 className="text-2xl font-medium">Dashboard component</h3>;
 };
 
 export default Dashboard;
--- 9. Explore The Power of TanStack Query/module-8/src/pages/admin/ServiceList.tsx
+++ 9. Explore The Power of TanStack Query/module-8/src/pages/admin/ServiceList.tsx
@@ -10,13 +10,14 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Trash2 } from 'lucide-react';
 import { FormEvent, useState } from 'react';
 
 const ServiceList = () => {
   //* Add New Service Data
   const [serviceName, setServiceName] = useState('');
+  const queryClient = useQueryClient();
 
   const {
     mutateAsync,
@@ -32,9 +33,13 @@ const ServiceList = () => {
         },
       });
     },
+    onSuccess: async () => {
+      queryClient.invalidateQueries({
+        queryKey: ['services'],
+      });
+    },
   });
-
-  console.log({ isPostError, isSuccess });
+  // console.log({ isPostError, isSuccess });
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
@@ -49,10 +54,10 @@ const ServiceList = () => {
     // console.log(serviceData);
     await mutateAsync(serviceData);
 
-    console.log('done');
+    // console.log('done');
   };
 
-  //* Get Service Data
+  //* Get Service Data Using Hook
   const { isLoading, isError, services, error } = useGetServices();
 
   if (isLoading) {
@@ -63,65 +68,63 @@ const ServiceList = () => {
   }
 
   return (
-    <>
-      <Container className="p-0 mt-10 border rounded-2xl">
-        {/* NOTE: When using axios */}
+    <Container className="p-0 mt-5 border rounded-2xl">
+      {/* NOTE: When using axios */}
 
-        {/* {data?.data?.data?.map((item) => (
+      {/* {data?.data?.data?.map((item) => (
         <p key={item._id}>{item.name}</p>
       ))} */}
 
-        {/* NOTE: When using fetch */}
+      {/* NOTE: When using fetch */}
 
-        {/* {data?.data?.map((item) => (
+      {/* {data?.data?.map((item) => (
         <p key={item._id}>{item.name}</p>
       ))} */}
 
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="font-semibold text-black">Name</TableHead>
-              <TableHead className="font-semibold text-black">
-                Description
-              </TableHead>
-              <TableHead className="font-semibold text-black">Price</TableHead>
-              <TableHead className="font-semibold text-black">Action</TableHead>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="font-semibold text-black">Name</TableHead>
+            <TableHead className="font-semibold text-black">
+              Description
+            </TableHead>
+            <TableHead className="font-semibold text-black">Price</TableHead>
+            <TableHead className="font-semibold text-black">Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {services.map((service: TService) => (
+            <TableRow key={service._id}>
+              <TableCell>{service.name}</TableCell>
+              <TableCell>{service.description}</TableCell>
+              <TableCell>${service.price}</TableCell>
+              <TableCell>
+                <Button variant="destructive" className="p-2">
+                  <Trash2 />
+                </Button>
+              </TableCell>
             </TableRow>
-          </TableHeader>
-          <TableBody>
-            {services.map((service: TService) => (
-              <TableRow key={service._id}>
-                <TableCell>{service.name}</TableCell>
-                <TableCell>{service.description}</TableCell>
-                <TableCell>${service.price}</TableCell>
-                <TableCell>
-                  <Button variant="destructive" className="p-2">
-                    <Trash2 />
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TableCell colSpan={3}>Total Services</TableCell>
-              <TableCell>{services.length}</TableCell>
-            </TableRow>
-          </TableFooter>
-        </Table>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={3}>Total Services</TableCell>
+            <TableCell>{services.length}</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
 
-        <div>
-          <form onSubmit={handleSubmit} className="my-10 text-center ">
-            <input
-              type="text"
-              className="mr-3 border-2 rounded border-primary "
-              onChange={(e) => setServiceName(e.target.value)}
-            />
-            <Button type="submit">Submit</Button>
-          </form>
-        </div>
-      </Container>
-    </>
+      <div>
+        <form onSubmit={handleSubmit} className="my-10 text-center ">
+          <input
+            type="text"
+            className="mr-3 border-2 rounded border-primary "
+            onChange={(e) => setServiceName(e.target.value)}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </div>
+    </Container>
   );
 };
 
