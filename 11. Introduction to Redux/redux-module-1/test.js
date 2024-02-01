b138b65d76b3b478e072952355a8f01869298517
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-02-01 09:22:28 +0600

    11-7: JavaScript Mutation and Function Currying

---
+++ 11. Introduction to Redux/redux-module-1/test.js
@@ -0,0 +1,16 @@
+//* Function Currying (Contains previous function reference)
+
+const add = (a) => {
+  return (b) => {
+    return (c) => {
+      return a * (b + c);
+    };
+  };
+};
+
+// console.log(add(1)(1)(1));
+const level = add(5);
+console.log(level(2)(2));
+console.log(level(5)(6));
+console.log(level(1)(3));
+console.log(level(8)(5));
