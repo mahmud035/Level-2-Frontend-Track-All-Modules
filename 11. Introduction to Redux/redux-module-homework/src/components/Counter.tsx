91091016bfc9c6d4f4e98714e2088ed4d3203361
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-02-01 15:47:16 +0600

    redux module-11 homework done

--- 11. Introduction to Redux/redux-module-homework/src/components/Counter.tsx
+++ 11. Introduction to Redux/redux-module-homework/src/components/Counter.tsx
@@ -7,11 +7,15 @@ import {
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 
 const Counter = () => {
-  const { count } = useAppSelector((state) => state.counter);
+  const { count, boxCount } = useAppSelector((state) => state.counter);
   const dispatch = useAppDispatch();
 
+  const boxArray = new Array(boxCount).fill(boxCount);
+  console.log(boxArray);
+  let index = 0;
+
   return (
-    <div className="flex items-center justify-center w-full h-screen">
+    <div className="flex flex-col items-center justify-center w-full h-screen gap-4">
       <div className="p-10 border border-purple-300 rounded bg-slate-50">
         <span className="block w-full mb-10 text-3xl font-semibold text-center text-teal-500">
           {count}
@@ -42,6 +46,18 @@ const Counter = () => {
             Decrement By 5
           </button>
         </div>
+      </div>
+      {/* Box */}
+      <div className="flex items-center justify-center w-full gap-3">
+        {boxArray.length > 0 &&
+          boxArray.map(() => (
+            <div
+              key={index++}
+              className="w-10 h-10 p-2 font-medium text-center text-white rounded bg-violet-400"
+            >
+              {index + 1}
+            </div>
+          ))}
       </div>
     </div>
   );
--- 11. Introduction to Redux/redux-module-homework/src/redux/features/counter/counterSlice.tsx
+++ 11. Introduction to Redux/redux-module-homework/src/redux/features/counter/counterSlice.tsx
@@ -2,10 +2,12 @@ import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 export type CounterStateType = {
   count: number;
+  boxCount: number;
 };
 
 const initialState: CounterStateType = {
   count: 0,
+  boxCount: 0,
 };
 
 export const counterSlice = createSlice({
@@ -14,14 +16,39 @@ export const counterSlice = createSlice({
   reducers: {
     increment: (state) => {
       state.count = state.count + 1;
+
+      if (state.count % 5 === 0) {
+        state.boxCount = state.boxCount + 1;
+      }
     },
     decrement: (state) => {
+      if (state.count === 0) {
+        alert(`Negative value isn't allowed!`);
+        return state;
+      }
+
+      if (state.count % 5 === 0) {
+        state.boxCount = state.boxCount - 1;
+      }
+
       state.count = state.count - 1;
     },
     incrementByAmount: (state, action: PayloadAction<number>) => {
       state.count = state.count + action.payload;
+
+      if (state.count % 5 === 0 || state.count % 5 >= 1) {
+        state.boxCount = state.boxCount + 1;
+      }
     },
     decrementByAmount: (state, action) => {
+      if (state.count < action.payload.value) {
+        alert(`Negative value isn't allowed!`);
+        return state;
+      }
+
+      if (state.count % 5 === 0 || state.count % 5 >= 1) {
+        state.boxCount = state.boxCount - 1;
+      }
       state.count = state.count - action.payload.value;
     },
   },
