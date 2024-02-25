7a7a49b804657249885d6a6f8030b0361551afeb
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-02-25 21:25:26 +0600

    7-3 Gesture - Hover And Tap

--- 7. Introduction To Framer Motion/framer-motion-introduction/src/App.tsx
+++ 7. Introduction To Framer Motion/framer-motion-introduction/src/App.tsx
@@ -1,6 +1,6 @@
-import Lesson1 from './pages/Lesson1';
+// import Lesson1 from './pages/Lesson1';
 // import Lesson2 from './pages/Lesson2';
-// import Lesson3 from './pages/Lesson3';
+import Lesson3 from './pages/Lesson3';
 // import Lesson4 from './pages/Lesson4';
 // import Lesson5 from './pages/Lesson5';
 // import Lesson6 from './pages/Lesson6';
@@ -15,9 +15,9 @@ import Lesson1 from './pages/Lesson1';
 const App = () => {
   return (
     <div className="flex items-center justify-center w-full h-screen">
-      <Lesson1 />
+      {/* <Lesson1 /> */}
       {/* <Lesson2 /> */}
-      {/* <Lesson3 /> */}
+      <Lesson3 />
       {/* <Lesson4 /> */}
       {/* <Lesson5 /> */}
       {/* <Lesson6 /> */}
